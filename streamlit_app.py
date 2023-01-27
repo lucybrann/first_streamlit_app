19cc06017d699b7f90e1690b38a930d27461f9e0
lucybrann <123733516+lucybrann@users.noreply.github.com>
2023-01-27 11:16:25 +0000

    Update streamlit_app.py
    added an example fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 #change index
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-st.multiselect("Select your favourite fruits !! :", list(my_fruit_list.index))
+st.multiselect("Select your favourite fruits !! :", list(my_fruit_list.index), ['Strawberries'])
 
 #show nutritional info
 st.dataframe(my_fruit_list)
