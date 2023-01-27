2d37a6fd60cd5d2e2eb0deccf965b5eb3f47f092
lucybrann <123733516+lucybrann@users.noreply.github.com>
2023-01-27 11:07:21 +0000

    Update streamlit_app.py
    multiselect yeehaw

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,15 @@ st.subheader("Cold Drinks")
 st.text("Orange Juice")
 st.text("Apple Juice")
 
+#create your own smoothie section
+#user interaction yeeehaw
 st.subheader("Build your own fruit smoothie ! ")
+
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#change index
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+st.multiselect("Select your favourite fruits !! :", list(my_fruit_list.index))
+
+#show nutritional info
 st.dataframe(my_fruit_list)
