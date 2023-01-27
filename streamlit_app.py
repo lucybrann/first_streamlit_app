b6c6039b5d91c3310d603dbe4710c06e1737261b
lucybrann <123733516+lucybrann@users.noreply.github.com>
2023-01-27 10:52:43 +0000

    Update streamlit_app.py
    headers

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,18 +3,18 @@ import streamlit as st
 
 st.title("Lucy's Lovely Brannigan Brunch")
 
-st.title("Brunch Menu ( ALL DAY<3 )")
+st.header("Brunch Menu ( ALL DAY<3 )")
 st.text("Caramelised Bacon Pancakes with yoghurt")
 st.text("Brannigan Special")
 
 
-st.title("Drinks Menu")
+st.header("Drinks Menu")
 
-st.subtitle("Hot Drinks")
+st.subheader("Hot Drinks")
 st.text("Breakfast Tea")
 st.text("Coffee")
 st.text("Sticky Toffee Pudding Hot Chocolate")
 
-st.subtitle("Cold Drinks")
+st.subheader("Cold Drinks")
 st.text("Orange Juice")
 st.text("Apple Juice")
