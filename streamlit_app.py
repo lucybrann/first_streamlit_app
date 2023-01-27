576e947649f3b7b689c8dfffeec1cded635a0c9a
lucybrann <123733516+lucybrann@users.noreply.github.com>
2023-01-27 10:51:50 +0000

    Update streamlit_app.py
    Changed menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,20 @@
 #import packages
 import streamlit as st
 
-st.title('My new healthy diner')
+st.title("Lucy's Lovely Brannigan Brunch")
+
+st.title("Brunch Menu ( ALL DAY<3 )")
+st.text("Caramelised Bacon Pancakes with yoghurt")
+st.text("Brannigan Special")
+
+
+st.title("Drinks Menu")
+
+st.subtitle("Hot Drinks")
+st.text("Breakfast Tea")
+st.text("Coffee")
+st.text("Sticky Toffee Pudding Hot Chocolate")
+
+st.subtitle("Cold Drinks")
+st.text("Orange Juice")
+st.text("Apple Juice")
